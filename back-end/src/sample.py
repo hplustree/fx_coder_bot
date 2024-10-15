d7c0e44c93d13891742754e95f533e1d9ed07e74
het28052001 <het.shah@fxis.ai>
2024-10-15 12:55:30 +0530

    Added sample file for testing purpose

---
+++ back-end/src/sample.py
@@ -0,0 +1,85 @@
+import base64
+import openai
+import os
+from openai import OpenAI
+from generate_file_name_and_extension import generate_FileName_and_extension
+from dotenv import load_dotenv
+
+load_dotenv()
+
+open_ai_key = os.environ["OPENAI_API_KEY"]
+
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+# Function to send prompt and image to OpenAI API
+def get_code_from_image(image_path, user_prompt):
+    # Getting the base64 string of the image
+    base64_image = encode_image(image_path)
+
+    # Initialize the OpenAI client
+    client = OpenAI(api_key=open_ai_key)
+
+    # Create the initial chat completion request
+    messages = [
+        {
+            "role": "system",
+            "content": (
+                "You are a coding assistant. Your response should strictly contain only code snippets. "
+                "Do not provide any explanations, comments, or additional text. "
+                "Strictly avoid using any code fencing, such as ``` or language tags like ```javascript. "
+                "If there are multiple code files, separate them with clear markers like '---'. "
+                "Only return the necessary code."
+
+            )
+        },
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": user_prompt},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{base64_image}",
+                        "detail": "high"
+                    }
+                }
+            ]
+        }
+    ]
+
+    max_tokens = os.environ["MAX_TOKENS"]
+    response_text = ""
+    while True:
+        response = client.chat.completions.create(
+            model=os.environ["MODEL"],
+            messages=messages,
+            max_tokens=int(max_tokens)
+        )
+
+        response_text += response.choices[0].message.content
+
+        if response.choices[0].finish_reason != "length":
+            break
+
+        messages.append({
+            "role": "assistant",
+            "content": response.choices[0].message.content
+        })
+
+        messages.append({
+            "role": "user",
+            "content": "Continue from where you left off."
+        })
+
+    return response_text
+
+# Example usage
+# image_path = "/home/dhruvi/Desktop/Pr_generation_bot/back-end/src/Screenshot from 2024-10-09 16-16-38.png"
+# user_prompt = "Give me react js code of the image uploaded"
+# response = get_code_from_image(image_path, user_prompt)
+# print(response)
+# file_name, file_extension = generate_FileName_and_extension(response)
+# print(file_name, file_extension)
